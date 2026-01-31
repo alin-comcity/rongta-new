d113bac4657cc4bcfd165c556caeb5ee40cfcff0
Masum <masum@Masums-Mac-mini.local>
2026-01-31 14:11:23 +0600

    Fix it Alin

--- app/pages/Privacy.vue
+++ app/pages/Privacy.vue
@@ -1,121 +1,149 @@
 <template>
-    <div class="py-12 px-10 container mx-auto">
+  <div class="py-12 px-10 container mx-auto">
+    <div class="flex flex-col items-center mt-8">
+      <h2 class="text-3xl font-bold uppercase tracking-wider">
+        Privacy Policy
+      </h2>
 
-        <div class="flex flex-col items-center mt-8">
-            <h2 class="text-3xl font-bold uppercase tracking-wider">Privacy Policy</h2>
-
-            <img src="/bt-i.png" class="mt-4" />
-        </div>
-
-        <h2 class="mt-10">This Privacy Policy describes how your personal information is collected, used, and shared
-            when you visit or make a purchase from rongta.com.bd</h2>
-        <br />
-        <br />
-
-        <h2 class="font-semibold">PERSONAL INFORMATION WE COLLECT</h2>
-        <br />
-        <br />
-
-        <p class="text-sm">
-            When you visit the Site, we automatically collect certain information about your device, including
-            information about your web browser, IP address, time zone, and some of the cookies that are installed on
-            your device. Additionally, as you browse the Site, we collect information about the individual web pages or
-            products that you view, what websites or search terms referred you to the Site, and information about how
-            you interact with the Site. We refer to this automatically-collected information as “Device Information”.
-            We collect Device Information using the following technologies:
-            <br />
-        <p>
-            <br />– “Cookies” are data files that are placed on your device or computer and often include an anonymous
-            unique identifier. For more information about cookies, and how to disable cookies, visit
-            http://www.allaboutcookies.org.
-        </p>
-        – “Log files” track actions occurring on the Site, and collect data including your IP address, browser type,
-        Internet service provider, referring/exit pages, and date/time stamps.
-        – “Web beacons”, “tags”, and “pixels” are electronic files used to record information about how you browse the
-        Site, along with basic user-demographic such as general location, age range, and gender, as is provided by
-        connected social media websites.
-        When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information
-        and Order Information.
-        <br />
-        <br />HOW DO WE USE YOUR PERSONAL INFORMATION?
-        <p>
-            We use the Order Information that we collect generally to fulfill any orders placed through the Site
-            (including processing your payment information, arranging for shipping, and providing you with invoices
-            and/or order confirmations). Additionally, we use this Order Information to:</p>
-        – Communicate with you;
-        – Screen our orders for potential risk or fraud; and
-        – When in line with the preferences you have shared with us, provide you with information or advertising
-        relating to our products or services.
-        – Analyze our customer demographic to bring you better products and deals.
-        – Create relevant marketing material for you and potential new customers.
-        – Do market research to improve our shipping/RMA centers.
-        We use the Device Information that we collect to help us screen for potential risk and fraud (in particular,
-        your IP address), and more generally to improve and optimize our Site (for example, by generating analytics
-        about how our customers browse and interact with the Site, and to assess the success of our marketing and
-        advertising campaigns).
-        <br />
-        <br />SHARING YOUR PERSONAL INFORMATION
-        <p>
-            We share your Personal Information with third parties to help us use your Personal Information, as described
-            above. We use Google Analytics to help us understand how our customers use the Site — you can read more
-            about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You
-            can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.
-            Finally, we may also share your Personal Information to comply with applicable laws and regulations, to
-            respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise
-            protect our rights.</p>
-        <br />
-        <br />BEHAVIOURAL ADVERTISING<p>
-            As described above, we use your Personal Information to provide you with targeted advertisements or
-            marketing communications we believe may be of interest to you. For more information about how targeted
-            advertising works, you can visit the Network Advertising Initiative’s (“NAI”) educational page at
-            http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.</p>
-        You can opt out of targeted advertising by using the links below:
-        – Facebook: https://www.facebook.com/settings/?tab=ads
-        – Google: https://www.google.com/settings/ads/anonymous
-        – Bing: https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads
-        Additionally, you can opt out of some of these services by visiting the Digital Advertising Alliance’s opt-out
-        portal at: http://optout.aboutads.info/.
-        <br />
-        <br />DO NOT TRACK
-        <p>
-            Please note that we do not alter our Site’s data collection and use practices when we see a Do Not Track
-            signal from your browser.
-            Please note that your information may be transferred outside of Bangladesh.</p>
-        <br />
-        <br />DATA RETENTION
-        <p>
-            When you place an order through the Site, we will maintain your Order Information for our records unless and
-            until you ask us to delete this information.
-            <br />
-        </p>
-        <br />CHANGES
-        We may update this privacy policy from time to time in order to reflect, for example, changes to our practices
-        or for other operational, legal or regulatory reasons.
-        <br />
-        <br />CONTACT US
-        <p>
-            For more information about our privacy practices, if you have questions, or if you would like to make a
-            complaint, please contact us by e mail at (info@rongta.com.bd)</p>
-        </p>
+      <img src="/bt-i.png" class="mt-4" />
     </div>
-</template>
 
+    <h2 class="mt-10">
+      This Privacy Policy describes how your personal information is collected,
+      used, and shared when you visit or make a purchase from rongta.com.bd
+    </h2>
+
+    <h2 class="font-semibold py-8">PERSONAL INFORMATION WE COLLECT</h2>
+
+    <p class="text-sm py-4">
+      When you visit the Site, we automatically collect certain information
+      about your device, including information about your web browser, IP
+      address, time zone, and some of the cookies that are installed on your
+      device. Additionally, as you browse the Site, we collect information about
+      the individual web pages or products that you view, what websites or
+      search terms referred you to the Site, and information about how you
+      interact with the Site. We refer to this automatically-collected
+      information as “Device Information”. We collect Device Information using
+      the following technologies:
+    </p>
+
+    <p class="py-4">
+      – “Cookies” are data files that are placed on your device or computer and
+      often include an anonymous unique identifier. For more information about
+      cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
+    </p>
+    <p class="py-4">
+      - “Log files” track actions occurring on the Site, and collect data
+      including your IP address, browser type, Internet service provider,
+      referring/exit pages, and date/time stamps. - “Web beacons”, “tags”, and
+      “pixels” are electronic files used to record information about how you
+      browse the Site, along with basic user-demographic such as general
+      location, age range, and gender, as is provided by connected social media
+      websites. When we talk about “Personal Information” in this Privacy
+      Policy, we are talking both about Device Information and Order
+      Information.
+    </p>
+
+    <h2 class="py-4">HOW DO WE USE YOUR PERSONAL INFORMATION?</h2>
+    <p>
+      We use the Order Information that we collect generally to fulfill any
+      orders placed through the Site (including processing your payment
+      information, arranging for shipping, and providing you with invoices
+      and/or order confirmations). Additionally, we use this Order Information
+      to:
+    </p>
+
+    <p class="py-4">
+      - Communicate with you; - Screen our orders for potential risk or fraud;
+      and - When in line with the preferences you have shared with us, provide
+      you with information or advertising relating to our products or services.
+      - Analyze our customer demographic to bring you better products and deals.
+      - Create relevant marketing material for you and potential new customers.
+      - Do market research to improve our shipping/RMA centers. We use the
+      Device Information that we collect to help us screen for potential risk
+      and fraud (in particular, your IP address), and more generally to improve
+      and optimize our Site (for example, by generating analytics about how our
+      customers browse and interact with the Site, and to assess the success of
+      our marketing and advertising campaigns).
+    </p>
+
+    <h2 class="py-4">SHARING YOUR PERSONAL INFORMATION</h2>
+    <p class="py-4">
+      We share your Personal Information with third parties to help us use your
+      Personal Information, as described above. We use Google Analytics to help
+      us understand how our customers use the Site — you can read more about how
+      Google uses your Personal Information here:
+      https://www.google.com/intl/en/policies/privacy/. You can also opt-out of
+      Google Analytics here: https://tools.google.com/dlpage/gaoptout. Finally,
+      we may also share your Personal Information to comply with applicable laws
+      and regulations, to respond to a subpoena, search warrant or other lawful
+      request for information we receive, or to otherwise protect our rights.
+    </p>
+
+    <h2 class="py-4">BEHAVIOURAL ADVERTISING</h2>
+
+    <p class="py-4">
+      As described above, we use your Personal Information to provide you with
+      targeted advertisements or marketing communications we believe may be of
+      interest to you. For more information about how targeted advertising
+      works, you can visit the Network Advertising Initiative’s (“NAI”)
+      educational page at
+      http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
+    </p>
+    <p class="py-4">
+      You can opt out of targeted advertising by using the links below: -
+      Facebook: https://www.facebook.com/settings/?tab=ads - Google:
+      https://www.google.com/settings/ads/anonymous - Bing:
+      https://advertise.bingads.microsoft.com/en-us/resources/policies/personalized-ads
+      Additionally, you can opt out of some of these services by visiting the
+      Digital Advertising Alliance`s opt-out portal at:
+      http://optout.aboutads.info/.
+    </p>
+
+    <h2 class="py-4">DO NOT TRACK</h2>
+    <p class="py-4">
+      Please note that we do not alter our Site`s data collection and use
+      practices when we see a Do Not Track signal from your browser. Please note
+      that your information may be transferred outside of Bangladesh.
+    </p>
+
+    <h2 class="py-4">DATA RETENTION</h2>
+    <p class="py-4">
+      When you place an order through the Site, we will maintain your Order
+      Information for our records unless and until you ask us to delete this
+      information.
+    </p>
+    <h2 class="py-4">CHANGES</h2>
+    <p>
+      We may update this privacy policy from time to time in order to reflect,
+      for example, changes to our practices or for other operational, legal or
+      regulatory reasons.
+    </p>
+
+    <h2 class="py-4">CONTACT US</h2>
+    <p>
+      For more information about our privacy practices, if you have questions,
+      or if you would like to make a complaint, please contact us by e mail at
+      (info@rongta.com.bd)
+    </p>
+  </div>
+</template>
 
 <script>
 export default {
-    head: {
-        title: 'Rongta Bangladesh | Privacy Policy',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            {
-                hid: 'description',
-                name: 'Privacy Policy of Rongta BD Website',
-                content: 'This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from rongta.com.bd'
-            }
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav.png' }]
-    }
-}
-
+  head: {
+    title: "Rongta Bangladesh | Privacy Policy",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "Privacy Policy of Rongta BD Website",
+        content:
+          "This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from rongta.com.bd",
+      },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/fav.png" }],
+  },
+};
 </script>
